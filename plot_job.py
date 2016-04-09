d4dffa75973160e348dac7d9e16ec6421d3efd87
Muhummad Patel <muhummad.patel@gmail.com>
2016-04-09 19:52:03 +0200

    Convert to grayscale later. Converting too early reduced the image quality.

--- plot_job.py
+++ plot_job.py
@@ -16,10 +16,7 @@ class PlotJob:
     def __load_image(self, filename):
         return cv2.imread(filename)
 
-    def __prepare_image(self, img):
-        # convert to grayscale
-        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    def __prepare_image(self, image):
         # resize to fit plottable area
         image_width = image.shape[1]
         image_height = image.shape[0]
@@ -40,8 +37,11 @@ class PlotJob:
         im2 = palette[image]  # Applying palette on image
         im2 = cv2.convertScaleAbs(im2) # Converting image back to uint8
 
-        # cv2.imshow("im", im2)
-        # cv2.waitKey(0)
+        # convert to grayscale
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow("im", im2)
+        cv2.waitKey(0)
 
         shade_values = np.unique(im2) # get sorted shade values
         # print shade_values
