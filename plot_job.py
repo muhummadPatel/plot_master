631c5e55c1fb99769b094990f91f130ebcb1f276
Muhummad Patel <muhummad.patel@gmail.com>
2016-08-26 15:53:36 +0200

    Format code to fix pep8 warnings.

--- plot_job.py
+++ plot_job.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 class PlotJob:
 
     def __init__(self, plot_width, plot_height, shades, filename):
@@ -29,25 +30,25 @@ class PlotJob:
             r = min(width_scale, height_scale)
             dim = (int(image_width * r), int(image_height * r))
             # perform the actual resizing of the image and show it
-            image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+            image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
         n = self.shades    # Number of levels of quantization
         indices = np.arange(0, 256)   # List of all colors
-        divider = np.linspace(0, 255, n + 1)[1] # we get a divider
-        quantiz = np.int0(np.linspace(0, 255, n)) # we get quantization colors
-        color_levels = np.clip(np.int0(indices / divider), 0, n - 1) # color levels 0,1,2..
-        palette = quantiz[color_levels] # Creating the palette
-        im2 = palette[image]  # Applying palette on image
-        im2 = cv2.convertScaleAbs(im2) # Converting image back to uint8
+        divider = np.linspace(0, 255, n + 1)[1]  # we get a divider
+        quantiz = np.int0(np.linspace(0, 255, n))  # we get quantization colors
+        color_levels = np.clip(np.int0(indices / divider), 0, n - 1)  # color levels 0,1,2..
+        palette = quantiz[color_levels]  # Creating the palette
+        im2 = palette[image]   # Applying palette on image
+        im2 = cv2.convertScaleAbs(im2)  # Converting image back to uint8
 
-        # cv2.imshow("im", im2)
-        # cv2.waitKey(0)
+        cv2.imshow("im", im2)
+        cv2.waitKey(0)
 
-        shade_values = np.unique(im2) # get sorted shade values
+        shade_values = np.unique(im2)  # get sorted shade values
         # print shade_values
         # replace the shade vlaues with the shade index
         for i in range(self.shades):
-            im2[im2==shade_values[i]] = self.shades - (i + 1)
+            im2[im2 == shade_values[i]] = self.shades - (i + 1)
 
         # print im2
         # im2.fill(255)
@@ -58,22 +59,22 @@ class PlotJob:
         # cv2.destroyAllWindows()
 
     def __get_sequences(self, val, arr):
-    	sequences = []
+        sequences = []
         start = count = 0
-    	for i in range(len(arr)):
-    		if arr[i] >= val:
-    			if count == 0:
-    				start = i
-    			count += 1
-    		else:
-    			if count != 0:
-    				sequences.append((start, count))
-    			count = 0
+        for i in range(len(arr)):
+            if arr[i] >= val:
+                if count == 0:
+                    start = i
+                count += 1
+            else:
+                if count != 0:
+                    sequences.append((start, count))
+                count = 0
 
         if arr[-1] >= val:
-        	sequences.append((start, count))
+            sequences.append((start, count))
 
-    	return sequences
+        return sequences
 
     def __generate_lines(self, img):
         width = img.shape[1]
@@ -83,14 +84,24 @@ class PlotJob:
         h_lines = []
         for row in range(height):
             seq = self.__get_sequences(1, img[row])
-            row_lines = [(i[0], row, i[0] + i[1], row) for i in seq]
+
+            if row % 2 == 0:
+                row_lines = [(i[0], row, i[0] + i[1], row) for i in seq]
+            else:
+                row_lines = [(i[0] + i[1], row, i[0], row) for i in seq[::-1]]
+
             h_lines.extend(row_lines)
         self.lines.extend(h_lines)
 
         # vertical lines (shade 2)
         v_lines = []
         for col in range(width):
-            seq = self.__get_sequences(2, img[:,col])
-            col_lines = [(col, i[0], col, i[0] + i[1]) for i in seq]
+            seq = self.__get_sequences(2, img[:, col])
+
+            if col % 2 == 0:
+                col_lines = [(col, i[0], col, i[0] + i[1]) for i in seq]
+            else:
+                col_lines = [(col, i[0] + i[1], col, i[0]) for i in seq[::-1]]
+
             v_lines.extend(col_lines)
         self.lines.extend(v_lines)
--- plot_master.py
+++ plot_master.py
@@ -6,6 +6,7 @@ from plot_job import PlotJob
 # TODO: show the user the list of available ports (using pyserial) and let them
 # choose which to connect to
 
+
 def plot(lines):
     ser = serial.Serial()
     try:
@@ -28,30 +29,32 @@ def plot(lines):
             print code,
 
             response = ser.readline()
-            retries = 1;
+            retries = 1
             # print "X> " + response
             while response.find("x") < 0:
                 response = ser.readline()
                 retries += 1
 
-                if retries > 5:
+                if retries > 6:
                     print "no response, assume ok\n",
                     break
                 # print "X> " + response
-        ser.write("origin")
-        print "DONE"
+        ser.write("origin\n")
+        print "DONE. Finishing up..."
+        time.sleep(10)
     except KeyboardInterrupt:
         print "Error: KeyboardInterrupt"
     except serial.SerialException:
         print "Error: SerialException"
     finally:
         if ser.is_open:
+            print "closing connection..."
             ser.close()
             print "connection closed"
 
 
 def main():
-    job = PlotJob(52, 52, 3, "img/lenna.png")
+    job = PlotJob(130, 130, 3, "img/me.jpg")
     plot(job.lines)
 
 if __name__ == '__main__':
